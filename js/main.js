60c75fefe0c0505d232fd6b46255e9a399a74dde
Braxton Toomer <braxton.toomer@gmail.com>
2019-08-09 14:56:25 -0700

    html and css set up

---
+++ js/main.js
@@ -0,0 +1,5 @@
+//Define Constants (All upper-case identifiers)
+//Define Application-wide variables (State)
+//Identify Cached DOM Element Variables
+//Identify Event Listeners
+//Functions (init function to initialize/reset; render function for state variables to DOM)
